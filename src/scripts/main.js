bcc83568a788f4e6d5aa8c6d8482fe548347a3bb
ChrisShie <chrisshinta16@yahoo.co.id>
2017-06-14 12:53:00 +0700

    view fullscreen , choice align left, theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
     cube = require('bespoke-theme-cube'),
+    beach = require('bespoke-theme-beachday'),
     keys = require('bespoke-keys'),
+    click = require('bespoke-click'),
     touch = require('bespoke-touch'),
     bullets = require('bespoke-bullets'),
+    state = require('bespoke-state'),
     backdrop = require('bespoke-backdrop'),
     scale = require('bespoke-scale'),
     hash = require('bespoke-hash'),
@@ -12,17 +15,24 @@ var bespoke = require('bespoke'),
 
 // Bespoke.js
 bespoke.from('article', [
-    cube(),
+    beach(),
     keys(),
+    click(),
     touch(),
-    bullets('li, .bullet'),
+    bullets('h3,li, .bullet'),
+    state(),
     backdrop(),
     scale(),
     hash(),
     progress(),
     forms()
 ]);
-
+bespoke.from('article', [
+    bespoke.plugins.state()
+]);
+bespoke.from('div.choice', [
+    bespoke.plugins.click()
+]);
 // Prism syntax highlighting
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
