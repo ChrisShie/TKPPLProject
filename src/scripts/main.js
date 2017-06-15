9824ed29befd4d560ef78d5db7690130533725d6
Silfi Langie <silfilangie04@gmail.com>
2017-06-15 07:47:31 +0700

    Pembuatan coding js untuk analisa keputusan

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,26 +30,63 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-var ans = [];
 
-// function getRadioValue(theRadioName) {
-//     var radio = document.getElementsByName(theRadioName);
-//     console.log(radio);
+// var ans = [];
+// var myAns = "";
+// function getRadioValue(radioName, ind) {
+//     var radio = document.getElementsByName(radioName);
 //     for (var i = 0; i < radio.length; i++) {
-//         console.log(radio[i]);
 //         radio[i].onclick = function() {
-//             if (radio[i].checked) {
-//                 return this.value;
-//             }
+//             ans[ind] = this.value;
+//             console.log(ans);
 //         }
 //     }
 // }
-function getRadioValue(radioName, ind) {
+// myAns=ans.join("");
+var ans=[];
+var tmp="";
+var hasil="";
+function getRadioValue(radioName,ind){
     var radio = document.getElementsByName(radioName);
     for (var i = 0; i < radio.length; i++) {
         radio[i].onclick = function() {
             ans[ind] = this.value;
-            console.log(ans);
+            tmp="";
+            for(var j=0;j<ans.length;j++){
+                tmp+=ans[j];
+            }
+            hasil=tmp;
+            console.log(hasil);
+            if (hasil === '111111' || hasil === '111112' || hasil === '111113' || hasil === '111211' || hasil === '111212'){
+                document.getElementById("rcmd").innerHTML="Take some candies from your house/workplace, the chocolate or maybe fruity flavored will be good.";
+            }
+            else if (hasil === '111121'|| hasil === '111221'|| hasil === '112122'){
+                document.getElementById("rcmd").innerHTML="You can go to buy some candies/biscuits from grocery store/minimart near your house/workplace.";
+            }
+            else if (hasil === '111122' || hasil === '111222' || hasil === '111332'|| hasil === '112123' || hasil === '112222' || hasil === '112322'){
+                document.getElementById("rcmd").innerHTML="You can go to buy some bread/soft drinks from grocery store/minimart near your house/workplace.";
+            }
+            else if (hasil === '111123' || hasil === '111223' || hasil === '112223' || hasil === '112323'){
+                document.getElementById("rcmd").innerHTML="You can go to buy some soft drinks/chocolate bars from grocery store/minimart near your house/workplace.";
+            }
+            else if (hasil === '111131' || hasil === '111221' || hasil === '111231' || hasil === '111331'){
+                document.getElementById("rcmd").innerHTML="You better just stay at home/your workplace and take some candies,the chocolate or maybe fruity flavored ones.";
+            }
+            else if (hasil === '111132' || hasil === '111133' || hasil === '111232' || hasil === '111233'){
+                document.getElementById("rcmd").innerHTML="You can go to eat toast with/without drinks at a cafe/restaurant, how about kaya/chocolate toast?";
+            }
+            else if (hasil === '111213' || hasil === '111333' || hasil === '112113' || hasil === '112213' || hasil === '112313'){
+                document.getElementById("rcmd").innerHTML="You can ask for bread or soft drinks delivery.";
+            }
+            else if (hasil === '112111' || hasil === '112112' || hasil === '112121' || hasil === '112211' || hasil === '112212'|| hasil === '112221'){
+                document.getElementById("rcmd").innerHTML="Take some candies from your house/workplace, any flavored will be good.";
+            }
+            else if (hasil === '112311' || hasil === '112312' || hasil === '112321' || hasil === '112331'){
+                document.getElementById("rcmd").innerHTML="You better just stay at home/your workplace and take some candies,any flavored.";
+            }
+            else if (hasil === '112331'){
+                document.getElementById("rcmd").innerHTML="You better just stay at home/your workplace and take some candies,any flavored.";
+            }
         }
     }
 }
@@ -59,7 +96,5 @@ getRadioValue("qa-3", 2);
 getRadioValue("qa-4", 3);
 getRadioValue("qa-5", 4);
 getRadioValue("qa-6", 5);
-var myAns = "";
-for (var i = 0; i < 6; i++) {
-    myAns += ans[i];
-}
+
+    
